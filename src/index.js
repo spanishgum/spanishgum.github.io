73921879ed7ff2369cadc0fd01ba52253bda104c
Adam Stallard <spanishgum@gmail.com>
2021-01-15 02:03:49 +0000

    prettier

--- src/index.js
+++ src/index.js
@@ -1,13 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(
-  <React.StrictMode>
-    Hi
-    </React.StrictMode>,
-  document.getElementById('root')
+  <React.StrictMode>Hi</React.StrictMode>,
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
